1f8ee95b5c8dcbae0353275df89f2b8aacb7b448
Luisa Tavares <luisatavares@estudante.ufscar.br>
2023-10-29 13:47:05 -0300

    add: mais funcionalidades polimorfismo

--- homeStudy/Heranca/main.cpp
+++ homeStudy/Heranca/main.cpp
@@ -19,4 +19,21 @@ int main(){       //quando usar um ponteiro, usar NEW PARA INSTANCIAS
     cout << "Circulo:\n";
     Forma* circ = new Circulo{0, 0, 10};
     circ->imprimirDados();
+
+    //vetor de formas diferentes
+    Forma* vetor_formas[3];
+    vetor_formas[0] = new Retangulo{10, 30, 40, 21};
+    vetor_formas[1] = new Forma{100, 140};
+    vetor_formas[2] = new Circulo{2.0, 0.54, 12};
+
+    //podem ser usados como tipos da classe de boas!!!
+    vetor_formas[2]->imprimirDados();
+    
+    //para cada forma nesse vetor de formas, imprima os dados
+    for(auto forma : vetor_formas){
+        cout<< "Forma: "<<endl;
+        forma->imprimirDados();
+        cout<< "\n\n";
+    }
+
 }
